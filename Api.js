1094dd3114d879da511d879e27216c4ddbd2766f
KeeganThompson <keegantcollege@gmail.com>
2024-04-09 17:41:34 -0400

    something

--- Api.js
+++ Api.js
@@ -25,3 +25,25 @@ export const postData = async (data) => {
     throw error; // Rethrow the error to handle it at the caller
   }
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmad2HSLajGi7HxBRCywJvlZGoDE8Gn8",
+  authDomain: "mobileui-f135d.firebaseapp.com",
+  projectId: "mobileui-f135d",
+  storageBucket: "mobileui-f135d.appspot.com",
+  messagingSenderId: "336410808732",
+  appId: "1:336410808732:web:8740266f1ecefc33a67859",
+  measurementId: "G-WWGSPEHNHJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
